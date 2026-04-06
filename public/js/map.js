1c2e89a1854f5d512ce1642961103be1c39a8437
TechRS <hmraihansakib@gmail.com>
2026-04-06 14:22:55 +0600

    update index logic

--- public/js/map.js
+++ public/js/map.js
@@ -5,9 +5,9 @@ const coordinates = window.geoData?.coordinates || [90.6547, 23.2250];
 
 const map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/standard',
+    style: 'mapbox://styles/mapbox/streets-v12',
     projection: 'globe',
-    zoom: 9,
+    zoom: 10,
     center: coordinates
 });
 
@@ -18,12 +18,13 @@ map.on('style.load', () => {
     map.setFog({});
 });
 
-
-const marker = new mapboxgl.Marker({ color: 'red' })
-    .setLngLat(coordinates)  
+new mapboxgl.Marker({ color: 'red' })
+    .setLngLat(coordinates)
     .setPopup(
         new mapboxgl.Popup({ offset: 25 })
-            .setHTML(`<h4>${window.geoData ? "" : "Location"}</h4>
-                      <p>Exact location will be provided after booking</p>`)
+            .setHTML(`
+                <h4>Location</h4>
+                <p>Exact location will be provided after booking</p>
+            `)
     )
     .addTo(map);
